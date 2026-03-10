0fea39b344ccdcf30828641d9f84dfb45733b92c
dvorychesh <dvorychesh@gmail.com>
2026-03-10 20:32:02 +0200

    chore: trigger Vercel rebuild

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,3 +13,4 @@ function createPrismaClient() {
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// Rebuild trigger: 1773167522
